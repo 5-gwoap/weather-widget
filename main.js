522098869f7c48d044b836eb8217b006a8cb8e35
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-01-01 12:17:52 -0800

    Electron app creates tray icon. weather displayed on hover. Window is hidden and shown on icon click.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const request = require('request');
 
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray} = require('electron')
 const path = require('path')
 const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+let appIcon = null
 
 // Check if user has set environment variables before continuing.
 if (!process.env.WEATHER_TOKEN) {
@@ -38,23 +39,22 @@ const getWeather = () => {
   const temp = body.main.temp;
 
   // TODO: create node or element and write to client dom. OR Load another page displaying weather.
-  console.log("<h1>" + desc + ' - ' + temp + "</h1>");
+  appIcon.setToolTip(desc + ' - ' + temp);
   });
 }
 
-
-function createWindow () {
+const createWindow = () => {
   // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600,
     // show: false
-  })
+  });
 
   // Hide window when focus lost.
   win.on('blur', () => {
     win.hide();
-  })
+  });
 
 //temp get weather
   getWeather();
@@ -64,7 +64,7 @@ function createWindow () {
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools.
   // win.webContents.openDevTools()
@@ -74,21 +74,29 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow();
+
+  appIcon = new Tray('./sample.png');
+
+  appIcon.on('click', () => {
+    win.isVisible() ? win.hide() : win.show();
+  });
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
@@ -96,9 +104,9 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
-  }
-})
+    createWindow();
+  };
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
