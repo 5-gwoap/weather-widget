a4a1e0133f716fa8e40b758d121077c259aaff94
Joshua Wu <joshua.wu@liferay.com>
2018-01-02 10:36:27 -0800

    Initial development for base frame of app. (#1)
    * Add menubar and gitignore for node modules
    
    * Testing electron build for weather widget
    
    * getWeather works with hard coded token and city id, but cannot prompt user to set env var yet.
    
    * Clean up index.js and index.html
    
    * Add editor config.
    
    * Update main.js. Building from electron scratch. Remove menubar.
    
    * Electron app creates tray icon. weather displayed on hover.
    Window is hidden and shown on icon click.
    
    * Start app minimized. Add date to tool tip to help debug if weather is being updated on hover. Add on mouse-enter listener to icon.
    
    * Remvoing useless file that was left over in last clean
    
    * Readding index.html and removing index.js
    
    * Setting the window to have no frame so that a windows user does not close it by pressing 'x'
    
    * Moving getWeather into an "on finish loading" event so that we can make
    a fresh API call when we open app is done loading initially. We can then
    pass this to the HMTL file which can now handle the input that being
    passed to it.
    Modifying index.html to now have an empty paragraph and a placeholder
    refresh button. The button is not wired up and the paragraph gets
    populated by the script tag that reads input from the electron event.
    
    * Remove undeeded boostrap cdn

---
+++ main.js
@@ -0,0 +1,126 @@
+const request = require('request');
+
+const { app, BrowserWindow, Tray } = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+let appIcon = null
+
+// Check if user has set environment variables before continuing.
+if (!process.env.WEATHER_TOKEN) {
+  // TODO: Call method that will create element printing that the user needs to set this variable before continuing.
+  console.log('\"WEATHER_TOKEN\" environment variable has not been set.');
+  process.exit(1);
+}
+
+const getWeather = (callback) => {
+  const options = {
+    method: 'GET',
+    url: 'http://api.openweathermap.org/data/2.5/weather',
+    qs: {
+      id: '5342992',
+      appid: process.env.WEATHER_TOKEN,
+      units: 'imperial'
+    },
+    headers: {
+      'Cache-Control': 'no-cache'
+    }
+  };
+
+  request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+
+    body = JSON.parse(body);
+
+    const desc = body.weather[0].main;
+    const temp = body.main.temp;
+    const tooltip = desc + ' - ' + temp + ' ' + Date();
+
+    // TODO: create node or element and write to client dom. OR Load another page displaying weather.
+    appIcon.setToolTip(tooltip);
+    callback(tooltip)
+  });
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    frame: false
+  });
+
+  // Hide window when focus lost.
+  win.on('blur', () => {
+    win.hide();
+  });
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  win.webContents.on('did-finish-load', () => {
+    //temp get weather
+    getWeather((weather) => {
+      console.log(weather);
+      win.webContents.send('weathernow', weather);
+    });
+  })
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createWindow();
+
+  appIcon = new Tray('./sample.png');
+
+  appIcon.on('click', () => {
+    win.isVisible() ? win.hide() : win.show();
+  });
+
+  // TODO reload weather on icon hover. Not sure if implemented yet.
+  appIcon.on('mouse-enter', () => {
+    console.log('hover event');
+    getWeather();
+  })
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  };
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
